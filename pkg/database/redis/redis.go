fdb18e438223f8c3ad2b97b386f361631c5cf978
GilahGlobal <99908305+GilahGlobal@users.noreply.github.com>
2026-03-27 15:51:50 +0100

    Update redis.go

--- pkg/database/redis/redis.go
+++ pkg/database/redis/redis.go
@@ -1,7 +1,4 @@
-/*package redis
-
-import (
-	"einvoice-access-point/pkg/config"package redis
+package redis
 
 import (
 	"einvoice-access-point/pkg/config"
@@ -30,62 +27,5 @@ func NewClient() *redis.Client {
 		Password: "",
 		DB:       0,
 	})
-	return client
-}
-	"log"
-
-	"github.com/go-redis/redis/v8"
-)
-
-type Redis struct {
-	Red *redis.Client
-}
-
-func NewRedisConnection(rdb *redis.Client) *Redis {
-	return &Redis{Red: rdb}
-}
-
-func (rdb *Redis) RedisDb() *redis.Client {
-	return rdb.Red
-}
-
-func NewClient() *redis.Client {
-	redisConfig := config.GetConfig().Redis
-
-	log.Println("Connecting to Redis:", redisConfig.REDIS_URL)
-
-	// ✅ Proper parsing (handles TLS, auth, etc)
-	opt, err := redis.ParseURL(redisConfig.REDIS_URL)
-	if err != nil {
-		log.Fatalf("Failed to parse Redis URL: %v", err)
-	}
-
-	client := redis.NewClient(opt)
-
-	return client
-}*/
-
-package redis
-
-import (
-	"log"
-	"os"
-
-	"github.com/go-redis/redis/v8"
-)
-
-func NewClient() *redis.Client {
-	redisURL := os.Getenv("REDIS_URL")
-	if redisURL == "" {
-		redisURL = "redis://redis:6379/0"
-	}
-
-	opt, err := redis.ParseURL(redisURL)
-	if err != nil {
-		log.Fatal("Failed to parse Redis URL:", err)
-	}
-
-	client := redis.NewClient(opt)
-	log.Println("Connected to Redis at", redisURL)
 	return client
 }
