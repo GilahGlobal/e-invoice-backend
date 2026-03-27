e346875c1f2560ba7de0838a7364dc74fd6dd98a
GilahGlobal <99908305+GilahGlobal@users.noreply.github.com>
2026-03-27 15:50:53 +0100

    Update redis.go

--- pkg/database/redis/redis.go
+++ pkg/database/redis/redis.go
@@ -1,7 +1,37 @@
 /*package redis
 
 import (
+	"einvoice-access-point/pkg/config"package redis
+
+import (
 	"einvoice-access-point/pkg/config"
+	"log"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Redis struct {
+	Red *redis.Client
+}
+
+func NewRedisConnection(rdb *redis.Client) *Redis {
+	return &Redis{Red: rdb}
+}
+
+func (rdb *Redis) RedisDb() *redis.Client {
+	return rdb.Red
+}
+
+func NewClient() *redis.Client {
+	redisConfig := config.GetConfig().Redis
+	log.Println(redisConfig.REDIS_URL)
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisConfig.REDIS_URL,
+		Password: "",
+		DB:       0,
+	})
+	return client
+}
 	"log"
 
 	"github.com/go-redis/redis/v8"
